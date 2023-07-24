c1f5a332dfb8c775f6d7cb55ee17e48bae1a4d9d
olayinka06-dev <olayinkaconsult06@gmail.com>
2023-07-24 01:56:52 -0700

    creating the step four page

---
+++ src/components/tab-components/StepFour.jsx
@@ -0,0 +1,13 @@
+
+// import React from 'react';
+
+// const StepFour = ({activePlan, overall}) => {
+
+//   return (
+//     <div>
+//       <h3>{overall[activePlan].title}</h3>
+//     </div>
+//   )
+// }
+
+// export default StepFour;
